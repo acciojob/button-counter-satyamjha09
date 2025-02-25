11951cc8b4c9db03040a66b302471035849664ac
satyamjha <satyamjhaactor07@gmail.com>
2025-02-25 20:58:53 +0530

    all pass

--- src/components/ButtonCounter.js
+++ src/components/ButtonCounter.js
@@ -1,16 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const ButtonCounter = () => {
-
-    const [count, setCount] = useState(0);
-
+  const [count, setCount] = useState(0);
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <p> Button clicked {count} times </p>
-        <button onClick={() => setCount(count + 1)}> Click me </button>
+      <p>Button clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
     </div>
-  )
-}
+  );
+};
 
-export default ButtonCounter
+export default ButtonCounter;
